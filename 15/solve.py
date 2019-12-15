14788b02edd8d78af1e729cf6bd40c0e4078b742
Petr Vaněk <arkamar@atlas.cz>
2019-12-15 21:56:24 +0100

    15: udpate

--- 15/solve.py
+++ 15/solve.py
@@ -5,18 +5,19 @@ import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, flags, rc):
     print("[+] Connection success")
-    client.subscribe('#', qos = 1)
     client.subscribe('$SYS/#')
+    client.subscribe('#')
 
 def on_message(client, userdata, msg):
     print('[+] Topic: %s - Message: %s' % (msg.topic, msg.payload))
 
-client = mqtt.Client(client_id = "MqttClient")
+def on_error(client, error):
+    print('[-] Error:', error)
+
+client = mqtt.Client(transport = 'websockets')
 client.on_connect = on_connect
 client.on_message = on_message
+client.on_error = on_error
 
 client.connect('whale.hacking-lab.com', 9001, 60)
-while True:
-    time.sleep(1)
-    print('[+] loop')
-    client.loop()
+client.loop_forever()
